a007389cfbfd5ad51027fc57c60df12511658992
Cheke <cheke@example.com>
2021-10-22 00:54:14 -0300

    Crud estatico en arreglo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CrudApp from "./components/CrudApp";
+
+
+function App() {
+  return (
+    <>
+      <h1>Ejercicios con REACT</h1>
+      <CrudApp />
+    </>
+  );
+}
+
+export default App;
